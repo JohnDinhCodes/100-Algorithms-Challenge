6213907c3851e20af83e1c65cf91aaab672805c9
John Dinh <johnduydinh@gmail.com>
2019-10-15 22:01:34 -0400

    complete addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,12 @@
 function addTwoDigits(n: any): number {
+    
+    let numArr = n.toString().split("");
 
+    const result = numArr.reduce((prev, next) => {
+        return parseInt(prev + next);
+    });
+
+    return result;
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
