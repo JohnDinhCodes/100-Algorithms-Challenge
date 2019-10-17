74694dc2e0da8a863e9c8f282f702b039e2fb7e0
John Dinh <johnduydinh@gmail.com>
2019-10-16 23:17:38 -0400

    complete adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,17 @@
 function adjacentElementsProduct(inputArray: number[]): number {
 
+    let max: number = inputArray[0] + inputArray[1];
+
+    for (let i = 0; i < inputArray.length; i++) {
+        for (let j = 1; j < inputArray.length; j++) {
+            if (inputArray[i] + inputArray[j] > max && i !== j) {
+                max = inputArray[i] + inputArray[j];
+            }
+        }
+    }
+
+    return max;
+
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
