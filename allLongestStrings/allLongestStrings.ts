5e4b0deff2f1c3e57c8a7494fea4efe29895e207
John Dinh <johnduydinh@gmail.com>
2019-10-18 21:45:52 -0400

    complete allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,22 @@
 function allLongestStrings(inputArray: string[]): string[] {
 
+    let max = 0;
+    let result = [];
+
+    for (let string of inputArray) {
+        if (string.length > max) {
+            max = string.length;
+        }
+    }
+
+    for (let string of inputArray) {
+        if (string.length === max) {
+            result.push(string);
+        }
+    }
+
+    return result;
+
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
