fd255e8c335ee99ad5e903fd7e958c64276e16ee
John Dinh <johnduydinh@gmail.com>
2019-10-24 22:04:29 -0400

    complete alternatingSums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,16 @@
 function alternatingSums(a: number[]): number[] {
+    let teamOne = a[0];
+    let teamTwo = 0;
 
+    for (let i = 1; i < a.length; i++) {
+        if (i % 2 === 0) {
+            teamOne += a[i];
+        } else {
+            teamTwo += a[i];
+        }
+    }
+
+    return [teamOne, teamTwo];
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
