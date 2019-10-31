b30face84f4634994cdbfd60ad2d84ca029dd298
John Dinh <johnduydinh@gmail.com>
2019-10-30 22:08:09 -0400

    complete arrayMaximalAdjacentDifferences

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,17 @@
 function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+    let max = 0;
+    inputArray.forEach((num, index) => {
+        const next = inputArray[index + 1] || null;
+        const previous = inputArray[index - 1] || null;
 
+        if (Math.abs(num - previous) > max) {
+            max = Math.abs(num - previous);
+        } else if (Math.abs(next - num) > max) {
+            max = Math.abs(next - num);
+        }
+    });
+
+    return max;
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
