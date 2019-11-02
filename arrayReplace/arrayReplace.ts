c521f32d53d1424a94c4fb4a7e667fa3395b9d73
John Dinh <johnduydinh@gmail.com>
2019-11-01 21:47:57 -0400

    complete arrayReplace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,7 @@
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+    return inputArray.map(num => {
+        return num === elemToReplace ? substitutionElem : num;
+    });
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
